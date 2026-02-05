fd1480a7acec273018d4e265ea1bd551dcb7ef57
kukicl03 <luka.kukic2003@gmail.com>
2026-02-06 00:18:06 +0100

    Central Server implementacija zapoceta. Kreirane reference sa ostalim slojevima.

--- Pametna Kuca/CentralServer/Server.cs
+++ Pametna Kuca/CentralServer/Server.cs
@@ -10,15 +10,19 @@ using System.IO;
 using System.Diagnostics;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
+using UserLibrary;
+using HomeAppliances;
 
 namespace CentralServer
 {
     public class Server
     {
-        static void Main(string[] args)
-        {
-            
+        TcpListener listerner;
+        UserLibrary.ClientL client;
+        List<Appliance> appliances;
+        List<string> logs = new List<string>();
+        int nextUdpPort = 6000;
 
-        }
+        
     }
 }
